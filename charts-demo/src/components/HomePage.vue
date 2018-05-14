880c0397fdffa5afe84d47aeba62a0d551831eb9
Mary Boyd <mary.boyd314@gmail.com>
2018-05-14 10:18:05 -0400

    changed doc lang

--- charts-demo/src/components/HomePage.vue
+++ charts-demo/src/components/HomePage.vue
@@ -3,30 +3,30 @@
     <section class="left">
       <h1 class="display-3">Chart Examples</h1>
       <p class="headline my-3">
-        This page demonstrates all of the charts, including the various charting JavaScript libraries, available in the JS Catalyst package.</p>
+        This page displays all of the charts, including the various charting JavaScript libraries, available in the JS Catalyst package.</p>
     </section>
     <section class="right">
       <div class="card">
         <div class="my-3 mb-5">
           <h3 class="title mb-2">To Use</h3>
-          <p>The libraries and their respective charts are located in the menu dropdown on the upper left. All of the charts respond to various color themes, which can be changed using the Themes dropdown in the navigation bar.</p>
+          <p>The libraries and their respective charts are located below. All of the charts respond to various color themes, which can be changed using the Themes dropdown in the navigation bar.</p>
           <p>In JS Catalyst, you can use pre-made themes, or easily create your own.</p>
         </div>
         <div class="my-3">
           <h3 class="title mb-2">D3 Charts</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi iaculis, nisl nec fermentum laoreet, augue nulla pulvinar risus, a sagittis justo turpis vel elit. Aliquam et est tincidunt Read more <a href="#"> tincidunt</a>.</p>
+          <p>The most popular JavaScript data visualization library. Charts are built using HTML, SVG, and CSS. <a href="https://d3js.org/" target="_blank">Read more</a>.</p>
         </div>
         <div class="my-3">
           <h3 class="title mb-2">Plotly Charts</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi iaculis, nisl nec fermentum laoreet, augue nulla pulvinar risus, a sagittis justo turpis vel elit. Aliquam et est tincidunt Read more <a href="#"> tincidunt</a>.</p>
+          <p>A declarative charting library with easy-to-use, beautifully styled visuals. <a href="https://plot.ly/" target="_blank">Read more</a>.</p>
         </div>
         <div class="my-3">
           <h3 class="title mb-2">Chartjs Charts</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi iaculis, nisl nec fermentum laoreet, augue nulla pulvinar risus, a sagittis justo turpis vel elit. Aliquam et est tincidunt Read more <a href="#"> tincidunt</a>.</p>
+          <p>A simple and flexible charting library. <a href="http://www.chartjs.org/" target="_blank">Read more</a>.</p>
         </div>
         <div class="my-3">
           <h3 class="title mb-2">Chartist Charts</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi iaculis, nisl nec fermentum laoreet, augue nulla pulvinar risus, a sagittis justo turpis vel elit. Aliquam et est tincidunt Read more <a href="#"> tincidunt</a>.</p>
+          <p>A flexible, responsive charting library. <a href="https://gionkunz.github.io/chartist-js/" target="_blank">Read more</a>.</p>
         </div>
       </div>
     </section>
