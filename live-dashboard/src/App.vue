d7be750fee441694e04a09d880e35c0d9074f36a
lattimoregay <david.lattimore-gay@eikospartners.com>
2018-07-17 09:09:29 -0400

    Added ability to screen grab

--- live-dashboard/src/App.vue
+++ live-dashboard/src/App.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+import html2canvas from 'html2canvas-webpack' // This needs to be part of a mixin / plugin
+
 import Dashboard from './components/Dashboard.vue'
 import Toolbar from './components/Toolbar.vue'
 
@@ -14,7 +16,49 @@ export default {
   components: {
     'dashboard': Dashboard,
     'toolbar': Toolbar
-  }
+  },
+  mounted() {
+      window.addEventListener("message", event=>{
+        debugger
+
+        if ( event.data.action !== undefined ) {
+          if ( event.data.action === 'bringToFront') {
+            let finWin = fin.desktop.Window.getCurrent()
+            finWin.restore()
+            finWin.bringToFront()
+            return
+          }
+        }
+
+    if( event.data.action !== undefined && event.data.action === 'screencapture') 
+    {
+        let body = window.document.body
+        html2canvas(body)
+        .then(canvas => {
+            try {
+                debugger
+
+              let data = canvas.toDataURL("img/png")
+
+              let obj = JSON.stringify(data)
+
+              event.source.postMessage({
+                action: 'screencapture',
+                width: canvas.width,
+                height: canvas.height,
+                data: obj
+              }, '*')
+            } catch (exception ) {
+              console.log(exception)
+            }
+        })
+        .catch(error => {
+          console.log(error)
+        });
+      }}
+      , false);
+
+  },
 }
 </script>
 
